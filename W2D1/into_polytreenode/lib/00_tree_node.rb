7b7f4d5ec80aec0a30e9d79164282df20518e8c4
KennyOLaw <kaylenlaw11@gmail.com>
2019-12-30 16:17:17 -0500

    update

--- W2D1/into_polytreenode/lib/00_tree_node.rb
+++ W2D1/into_polytreenode/lib/00_tree_node.rb
@@ -1,3 +1,31 @@
 class PolyTreeNode
+  attr_reader :value
+  attr_accessor :parent, :children
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
 
+  def parent=(parent_node)
+    @parent.children.delete(self) unless @parent.nil?
+    
+    @parent = parent_node
+    parent_node.children << self unless parent_node.nil?
+  end 
+
+  def inspect
+    self.value
+  end 
+
+  def add_child(child_node)
+    unless child_node.children.include?(self)
+        child_node.parent = self
+    end 
+
+  end 
+
+  def remove_child(child_node)
+
+  end 
 end
